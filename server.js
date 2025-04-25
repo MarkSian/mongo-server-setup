7216623c777b88d9ae271c4d821d67c1f707fe2b
MarkSian <merksianipar@gmail.com>
2025-04-25 15:35:31 -0600

    initialized

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+// Setups
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Routes
+
+
+// Error handling
+app.use((err, req, res, next) => {
+    res.status(500).json({ error: err.message });
+});
+
+// Listeners
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}
+);
